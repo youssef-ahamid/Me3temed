7711147a06039fb4c2ccadca879bf5506f1ebab6
Youssef Abdelhamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-04-13 22:28:35 +0200

    package

---
+++ package/me3temed.mjs
@@ -0,0 +1,40 @@
+class Me3temed {
+    #token;
+
+    constructor(options = {}) {
+        this.api = 'https://me3temed.samuraisoftware.house'
+        this.user = options.user;
+    }
+
+    OTP(user = {}) {
+        fetch(`${this.api}/otp`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',Ï
+            }, 
+            mode: 'cors',
+            body: JSON.stringify(user),
+        }).then((data) => {
+            this.#token = data.token;
+        }).catch((err) => {
+            console.error(err);
+        })
+    }
+
+    e3temed(creds = {}) {
+        const res = fetch(`${this.api}/login`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': this.#token,
+            }, 
+            mode: 'cors',
+            body: JSON.stringify({
+                otp: creds.otp
+            }),
+        });
+        return res;
+    }
+} 
+
+export default new Me3temed()
