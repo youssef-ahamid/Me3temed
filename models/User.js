9e687c061dd4c196651feccf8ca6a36f6c32ccea
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-29 16:43:41 +0700

    modify user model to accept tokens and passwords + user metadata

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     email: {type:String, unique:true},
+    refreshToken: String,
+    name: String,
+    img: String,
+    passwordHash: String,
     otp: {type:String},
     createdAt:{ type: Date, default: Date.now }
 });
