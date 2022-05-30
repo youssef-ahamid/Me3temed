da74cf6416071ca42cfb922780e33a5610f29d20
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-31 00:22:14 +0700

    a sexier client library

--- package/me3temed.mjs
+++ package/me3temed.mjs
@@ -2,39 +2,73 @@ class Me3temed {
     #token;
 
     constructor(options = {}) {
-        this.api = 'https://me3temed.samuraisoftware.house'
+        this.API_URL = 'https://me3temed.samuraisoftware.house/'
         this.user = options.user;
     }
 
-    OTP(user = {}) {
-        this.user = user;
-        fetch(`${this.api}/otp`, {
+    /**
+     * @description Send a post request to me3temed's server
+     * @param route: /path/to/endpoint
+     * @param data: request body
+     * @callback cb: executes after response success. Receives response data as a parameter
+     */
+    async post(route, data = {}, cb) {
+        const response = await fetch(`${this.API_URL + route}`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
+                'x-access-token': this.#token
             }, 
-            mode: 'cors',
-            body: JSON.stringify(this.user),
-        }).then((data) => {
+            body: JSON.stringify(data),
+        })
+        if(response.ok) {
+            let data = await response.json()
+            cb(data)
+        } else console.error(response.status, response.message)
+    }
+
+    /**
+     * @description Generate a one-time password for a user and email it to them 
+     * @param email: user's email
+     * @returns Data object with temporary token & sets token
+     */
+    async getOTP(email) {
+        await this.post('otp', email, (data) => {
             this.#token = data.token;
-        }).catch((err) => {
-            console.error(err);
+            return data
         })
     }
 
-    e3temed(creds = {}) {
-        const res = fetch(`${this.api}/login`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token': this.#token,
-            }, 
-            mode: 'cors',
-            body: JSON.stringify({
-                otp: creds.otp
-            }),
-        });
-        return res;
+    /**
+     * @description Login a user 
+     * @param strategy: Specify the login method used. Leave it blank to login using the token (if it exists)
+     * @oneOf ["", "otp", "password", or "link"]
+     * @param creds: The user's sign in credentials
+     * @creds { email, password, otp }
+     * @returns Data object with "user" property
+     */
+    async login(strategy = "", creds = {}) {
+        await this.post(`login/${strategy}`, creds, (data) => { 
+            this.user = data.user;
+            this.#token = data.token;
+            return data 
+        })
+    }
+
+    /**
+     * @description register a user 
+     * @param strategy: Leave it blank for default email and password sign up.
+     * @oneOf ["", "passwordless", "social"]
+     * @param userData: The user data used to create the account.
+     * @userData { email, password, name, img, meta, origin, isEmailVerified }
+     * @returns Data object with "user" property
+     */
+    async register(strategy = "", userData = {}) {
+        await this.post(`register/${strategy}`, userData, (data) => { 
+            this.user = data.user;
+            this.#token = data.token;
+            return data 
+        })
     }
 } 
 
