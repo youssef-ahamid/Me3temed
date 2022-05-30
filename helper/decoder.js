0c5c04746fe6bb2933843132f46b76d0847bcf78
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-30 20:09:00 +0700

    decode google jwt

---
+++ helper/decoder.js
@@ -0,0 +1,22 @@
+import { OAuth2Client } from "google-auth-library";
+/**
+ * @description Function to decode Google OAuth token
+ * @param token: string
+ * @returns ticket object
+ */
+export const getDecodedOAuthJwtGoogle = async (token) => {
+  const CLIENT_ID_GOOGLE = "{{ client_id }}";
+
+  try {
+    const client = new OAuth2Client(CLIENT_ID_GOOGLE);
+
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID_GOOGLE,
+    });
+
+    return ticket;
+  } catch (error) {
+    return { status: 500, data: error };
+  }
+};
