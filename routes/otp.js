fc9f1474f197c5b67b155cd79db59383ba916f4d
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-31 00:22:49 +0700

    otp route =: mess -> sex

--- routes/otp.js
+++ routes/otp.js
@@ -2,14 +2,14 @@ import express from "express";
 const router = express.Router();
 
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import fetch from "node-fetch";
 
 //Models
 import UserObject from "../models/User.js";
+import zaagel from "zaagel";
+import { s } from "../helper/utils.js";
 
 // Get OTP
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const { email, support } = req.body;
 
   // Generate an OTP
@@ -17,62 +17,26 @@ router.post("/", (req, res, next) => {
   const otp = num.toString();
 
   // Get hash and save to the database.
-  bcrypt.hash(otp, 10).then((hash) => {
-    const promise = UserObject.updateOne(
-      {
-        email,
-      },
-      {
-        email: email,
-        otp: hash,
-      },
-      {
-        upsert: true,
-      }
-    );
-    promise
-      .then((data) => {
-        let message = {
-          to: email,
-          subject: otp + " is your one time password.",
-          template: 'OTP',
-          data: {
-            otp,
-            support
-          }
-        };
-        fetch(`https://zaagel.samuraisoftware.house/mail`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          mode: "cors",
-          body: JSON.stringify(message),
-        }).then(() => {
-          // Prepare a token.
-          const payload = {
-            email,
-          };
-          const token = jwt.sign(payload, process.env.TEMPORARILYTOKENKEY, {
-            expiresIn: 900, // This token expires 15 minutes later.
-          });
+  const hash = await bcrypt.hash(otp, 10);
 
-          // Send a response.
-          res.status(200).json({
-            success: true,
-            message: "If your address is correct, you will receive an email!",
-            token: token,
-            //otp: otp
-            // The above line is added for the test. Uncomment it, when testing.
-          });
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          success: false,
-          message: "Something went wrong. Try again later.",
-        });
-      });
+  const user = await UserObject.updateOne(
+    { email },
+    { email, otp: hash },
+    { upsert: true }
+  );
+
+  zaagel.mail({
+    to: email,
+    subject: otp + " is your one time password.",
+    template: "OTP",
+    data: {
+      otp,
+      support,
+    },
   });
+
+  const token = user.genToken();
+  s(`OTP email sent to ${email}`, { token }, res);
 });
 
 export default router;
