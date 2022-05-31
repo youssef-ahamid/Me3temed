1ded59b86eebc2a2c409de583100df0f8a1a11fa
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-05-31 09:27:05 +0400

    login & update google users neatly OAuth2

--- routes/login.js
+++ routes/login.js
@@ -8,15 +8,94 @@ import verifyBaseToken from "./../middleware/basetoken.js";
 import { findUserByEmail, e, s, tryCatch } from "../helper/utils.js";
 import zaagel from "zaagel";
 
-// Verify token and login.
-router.post("/", verifyBaseToken, async (req, res, next) => {
-  const email = req.decoded_email;
-  const user = await findUserByEmail(email);
-  if (!user) e(400, `user with email ${email} not found`, res);
-  else {
-    const data = await user.login();
-    s("Log in successful!", data, res);
+import passport from "passport";
+import oauth20 from "passport-google-oauth20";
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { email: user.email, name: user.name });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+const GoogleStrategy = oauth20.Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "1086286750475-gqq1jk1jdepfkjbhronp81omr04g0cq0.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:8000/login/google-callback/",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      const { name, emails, displayName, photos } = profile;
+      const googleUser = {
+        email: emails[0].value,
+        isEmailVerified: emails[0].verified,
+        name: displayName,
+        img: photos[0].value,
+        data: { names: name },
+        refreshToken,
+      };
+      return cb(null, { googleUser, accessToken });
+    }
+  )
+);
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google-callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  async function (req, res) {
+    // Successful authentication, upsert user
+    const { googleUser } = req.user;
+    const origin = "google";
+
+    tryCatch(async () => {
+      // Check if user exists
+      const user = await findUserByEmail(googleUser.email);
+      if (!!user) {
+        if (
+          !!user.meta &&
+          !!user.meta.socials &&
+          user.meta.socials.map((social) => social.from).includes(origin)
+        )
+          s(
+            `User already registered this social login. Nothing new happened.`,
+            { user },
+            res
+          );
+        else {
+          await user.addSocialLogin(origin, googleUser.data);
+          if (googleUser.isEmailVerified && !user.isEmailVerified)
+            await user.verifyEmail();
+          s(`User updated! Added a social login from ${origin}`, { user }, res);
+        }
+      } else {
+        // Create user
+        const newUser = new User(googleUser);
+        const savedUser = await newUser.save();
+        s("User created!", { user: savedUser._doc }, res);
+      }
+    }, res);
+    // res.send("/success");
   }
+);
+
+router.get("/", function (req, res, next) {
+  res.render("login", {
+    signInOptions: ["google"],
+  });
 });
 
 // Verify token in link and login.
@@ -34,16 +113,17 @@ router.get("/link", verifyBaseToken, async (req, res, next) => {
 router.post("/link", async (req, res, next) => {
   const { email } = req.body;
   const user = await findUserByEmail(email);
-  if (!user) e(400, `user with email ${email} not found. please register instead.`, res);
+  if (!user)
+    e(400, `user with email ${email} not found. please register instead.`, res);
   else {
     const token = user.genToken();
-    const link = `http://localhost:8000/login/link?token=${token}`
+    const link = `http://localhost:8000/login/link?token=${token}`;
     zaagel.mail({
       to: email,
       subject: "Log in with link",
       template: "login-link",
-      data: { link }
-    })
+      data: { link },
+    });
     const data = await user.login();
     s("Log in successful!", data, res);
   }
@@ -77,7 +157,7 @@ router.post("/otp", verifyTempToken, async (req, res, next) => {
       if (!validOTP) e(401, "Incorrect OTP!", res);
       else {
         const data = await user.login();
-        if(!user.isEmailVerified) await user.verifyEmail();
+        if (!user.isEmailVerified) await user.verifyEmail();
         s("Log in successful!", data, res);
       }
     }, res);
